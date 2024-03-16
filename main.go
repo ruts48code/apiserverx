56815b94861fddba123ee1617a0301448c9546c1
Authapon Kongwan <authapon@gmail.com>
2024-03-16 22:57:32 +0700

    refactory to CronLoop

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ type (
 		OTP     OTPStruct     `yaml:"otp"`
 		Elogin  EloginStruct  `yaml:"elogin"`
 		Student StudentStruct `yaml:"student"`
+		ArsDB   ARSStruct     `yaml:"ars"`
 	}
 	OTPStruct struct {
 		Key      string `yaml:"key"`
@@ -33,49 +34,34 @@ type (
 		Update int `yaml:"update"`
 		Clean  int `yaml:"clean"`
 	}
+	ARSStruct struct {
+		DB     string `yaml:"db"`
+		Update int    `yaml:"update"`
+		Clean  int    `yaml:"clean"`
+	}
 )
 
 var (
 	conf Conf
 )
 
-func CleanTokenElogin() {
+func CronLoop(name string, web string, interval int) {
 	for {
-		log.Printf("Start Clean Token Elogin\n")
-		web := "https://api.rmutsv.ac.th/elogin/clean/" + otp.TimeOTPxHex([]byte(conf.OTP.Key), conf.OTP.Size)
-		log.Printf("web = %s\n", web)
-		utils.HTTPGet(web)
-		log.Printf("Clean Token Elogin Finnish\n")
-		time.Sleep(time.Duration(conf.Elogin.Clean) * time.Second)
-	}
-}
-
-func UpdateStudentProcess() {
-	for {
-		log.Printf("Start Update Student Process\n")
-		web := "https://api.rmutsv.ac.th/student/report/processalldata/" + otp.TimeOTPxHex([]byte(conf.OTP.Key), conf.OTP.Size)
-		log.Printf("web = %s\n", web)
-		utils.HTTPGet(web)
-		log.Printf("Update Student Process Finnish\n")
-		time.Sleep(time.Duration(conf.Student.Cache.Update) * time.Second)
-	}
-}
-
-func CleanStudentProcess() {
-	for {
-		log.Printf("Start Clean Student Process\n")
-		web := "https://api.rmutsv.ac.th/student/report/cleanalldata/" + otp.TimeOTPxHex([]byte(conf.OTP.Key), conf.OTP.Size)
-		log.Printf("web = %s\n", web)
-		utils.HTTPGet(web)
-		log.Printf("Clean Student Process Finnish\n")
-		time.Sleep(time.Duration(conf.Student.Cache.Clean) * time.Second)
+		log.Printf("Start %s\n", name)
+		webx := web + otp.TimeOTPxHex([]byte(conf.OTP.Key), conf.OTP.Size)
+		log.Printf("web = %s\n", webx)
+		utils.HTTPGet(webx)
+		log.Printf("%s Finnish\n", name)
+		time.Sleep(time.Duration(interval) * time.Second)
 	}
 }
 
 func main() {
 	utils.ProcessConfig("/etc/apiserver.yml", &conf)
-	go CleanTokenElogin()
-	go UpdateStudentProcess()
-	go CleanStudentProcess()
+	go CronLoop("Clean Token", "https://api.rmutsv.ac.th/elogin/clean/", conf.Elogin.Clean)
+	go CronLoop("Update Student Process", "https://api.rmutsv.ac.th/student/report/processalldata/", conf.Student.Cache.Update)
+	go CronLoop("Clean Student Process", "https://api.rmutsv.ac.th/student/report/cleanalldata/", conf.Student.Cache.Clean)
+	go CronLoop("Update ARS", "https://api.rmutsv.ac.th/ars/process/", conf.ArsDB.Update)
+	go CronLoop("Clean ARS", "https://api.rmutsv.ac.th/ars/clean/", conf.ArsDB.Clean)
 	select {}
 }
